c47c487ccd0dd5062584e01e4e887cb6a7ec3b63
ratul723 <ratul32742@gmail.com>
2024-04-30 21:16:04 +0600

    final commit

--- app.js
+++ app.js
@@ -5,9 +5,29 @@ menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+// let section = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header .navbar a');
+
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    // section.forEach(sec =>{
+
+    //   let top = window.scrollY;
+    //   let height = sec.offsetHeight;
+    //   let offset = sec.offsetTop - 150;
+    //   let id = sec.getAttribute('id');
+
+    //   if(top => offset && top < offset + height){
+    //     navLinks.forEach(links =>{
+    //       links.classList.remove('active');
+    //       document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+    //     })
+    //   }
+
+    // })
 }
 
 document.querySelector('#search-icon').onclick = () =>{
@@ -31,4 +51,29 @@ var swiper = new Swiper(".home-slider", {
       clickable: true,
     },
     loop:true,
+  });
+
+  var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    centeredSlides: true,
+    autoplay: {
+      delay: 6000,
+      disableOnInteraction: false,
+    },
+    loop:true,
+    breakpoints: {
+      0: {
+        slidesPreview: 1,
+      },
+      640: {
+        slidesPreview: 2,
+      },
+      768: {
+        slidesPreview: 2,
+      },
+      1024: {
+        slidesPreview: 3,
+      },
+
+    },
   });
